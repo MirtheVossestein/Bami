811a106fe30c8a4d8a46a413568c7c1e0b5f993d
BasieDEV <bashartkamp@gmail.com>
2024-06-12 08:20:35 +0200

    Added Colors

--- laravel/resources/views/login.blade.php
+++ laravel/resources/views/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="colors.css">
 </head>
 <body class="font-sans bg-gray-200">
     <div class="max-w-lg mx-auto p-4 bg-white rounded shadow-md mt-20">
@@ -18,7 +19,7 @@
                 <input type="password" id="password" name="password" required class="p-2 border border-gray-300 rounded">
             </div>
             <div class="flex flex-col">
-                <button type="submit" class="p-2 bg-green-500 text-white rounded cursor-pointer hover:bg-green-400 w-full">Login</button>
+                <button type="submit" class="p-2 olivegreen text-white rounded cursor-pointer hover:bg-green-400 w-full">Login</button>
             </div>
         </form>
     </div>
