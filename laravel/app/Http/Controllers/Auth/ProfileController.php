1b6810e6506c8a32ce4ddc1116c8a8b66936f7c6
BasieDEV <bashartkamp@gmail.com>
2024-06-13 14:20:52 +0200

    Added Login

---
+++ laravel/app/Http/Controllers/Auth/ProfileController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class ProfileController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        // Validate the form data
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // Attempt to log the user in
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+            // If successful, then redirect to their intended location
+            return redirect()->intended(route('index'));
+        } else {
+            // If unsuccessful, then redirect back to the login with form data
+            return redirect()->back()->withInput($request->only('email', 'remember'))->withErrors([
+                'login_error' => 'These credentials do not match our records.',
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
--- laravel/resources/views/login.blade.php
+++ laravel/resources/views/login.blade.php
@@ -3,9 +3,12 @@
 <head>
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="colors.css">
+    <link rel="stylesheet" type="text/css" href="font-ubuntu.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 </head>
-<body class="font-sans bg-gray-200">
+<body class=" ubuntu-regular font-sans bg-gray-200">
     <div class="max-w-lg mx-auto p-4 bg-white rounded shadow-md mt-20">
         <h2 class="text-2xl font-bold mb-5">Login</h2>
         <form action="/login" method="POST" class="space-y-4">
@@ -19,8 +22,11 @@
                 <input type="password" id="password" name="password" required class="p-2 border border-gray-300 rounded">
             </div>
             <div class="flex flex-col">
-                <button type="submit" class="p-2 olivegreen text-white rounded cursor-pointer hover:bg-green-400 w-full">Login</button>
+                <button type="submit" class="p-2 bg-green-800 text-white rounded cursor-pointer hover:bg-green-600 w-full">Login</button>
             </div>
+            @error('login_error')
+                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+            @enderror
         </form>
     </div>
 </body>
