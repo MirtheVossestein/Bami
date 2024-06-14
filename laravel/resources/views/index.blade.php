d792d0fa1e37c6fb23970fbb1d1e86be410d1dcb
MirtheVossestein <163519773+MirtheVossestein@users.noreply.github.com>
2024-06-14 11:26:52 +0200

    siiiu

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -7,20 +7,45 @@
         <title>Homepagina</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
         <script src="https://cdn.tailwindcss.com"></script>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+
         
         
     </head>
     <body class="h-screen flex flex-col">
-    <!-- Top section -->
-    <div class="h-1/2 bg-green-900 flex items-center justify-center">
-        <h1 class="text-white text-4xl font-bold">Green Top Section</h1>
-    </div>
-    <!-- Bottom section -->
-    <div class="h-1/2 bg-white flex items-center justify-center">
-        <h1 class="text-gray-800 text-4xl font-bold">White Bottom Section</h1>
-    </div>
-</body>
+    <div class="h-72 bg-green-800 flex items">
+    <a href="index.blade.php" class="z-20 py-2 px-5 text-white text-4xl font-bold">
+        <h1 class="ubuntu-light">BaMihuisjes.com</h1>
+    </a>
+        <div class="absolute z-20 top-4 right-80 py-1 px-16 right-2 shadow-lg bg-white rounded-lg p-2">
+                <h1 class="text-sm">Huisjes</h1>
+            </div>
 
+            <div class="absolute z-20 top-4 right-32 py-1 px-16 right-2 shadow-lg bg-white rounded-lg p-2">
+                <h1 class="text-sm">Contact</h1>
+            </div>
+
+            <div class="absolute z-20 top-4 py-1 px-6 right-6 shadow-lg bg-white rounded-lg p-2">
+                <h1 class="text-sm">Login </h1>
+            </div>
+
+</div>
+<div class="flex justify-center">
+    <div class="w-72 h-64 mb-4 mx-8">
+        <img class="rounded-lg object-scale-down w-full h-full object-left-bottom " src="{{ asset('images/Voorthuizen/HouseV.png') }}" alt="Voorthuizen">
+    </div>
+    <div class="w-72 h-58 mb-4 mx-8">
+        <img class="rounded-lg object-scale-down w-full h-full  object-left-bottom " src="{{ asset('images/Olburgen/HouseO.jpg') }}" alt="Olborgen">
+    </div>
+    <div class="w-72 h-64 mb-4 mx-8">
+        <img class="rounded-lg object-scale-down w-full h-full object-left-bottom " src="{{ asset('images/Hierden/HouseH.jpg') }}" alt="Afbeelding 3">
+    </div>
+</div>
+
+
+</body>
 </html>
